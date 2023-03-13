5c510efa05cf74df5f767acf00ec1056b237a22e
Projet Cool <projet.cool2022@gmail.com>
2023-03-13 09:43:15 -0400

    Add existing file

---
+++ src/main/java/com/coolerp/gestionstockapi/GestionstockapiApplication.java
@@ -0,0 +1,13 @@
+package com.coolerp.gestionstockapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionstockapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionstockapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coolerp/gestionstockapi/GestionstockapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coolerp.gestionstockapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionstockapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
